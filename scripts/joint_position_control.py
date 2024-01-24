9f1e91e34cd29ce3aadab842eb058c5f71dfbf31
aymenzayene <156333161+aymenzayene@users.noreply.github.com>
2024-01-24 16:01:07 +0100

    Update joint_position_control.py

--- scripts/joint_position_control.py
+++ scripts/joint_position_control.py
@@ -20,52 +20,25 @@ def clamp(value, min_value, max_value, joint_name):
     return value
 
 def perform_pick_and_place(cx, cy):
-    global bot
-   # Calculate dynamic waist position and clamp it within limits
+    # Calculate dynamic waist position with centroid coordinate and clamp it within limits
    #dynamic_waist = -0.79288521 + 0.00244743 * cx - 0.00032535 * cy
     dynamic_waist = -1.208291239413795 + 0.00258455697229715266 * cx + 0.00129699620668963693 * cy
-    dynamic_shoulder = -1.208291239413795- 0.004949365901311755 * cx + 0.0021293099284373407 * cy
-    dynamic_elbow = -1.20829139413795 + 0.0004456043414232663 * cx - 0.0016755823305856727 * cy
-    dynamic_wrist_angle = -0.0077659804792566955 -1.3953274884882079 * dynamic_shoulder  -0.8178526903149 * dynamic_elbow
-    dynamic_wrist_angle = clamp(dynamic_wrist_angle, -1.745329, 2.146755, "wrist_angle")
-
-    dynamic_forearm_roll=0
-    dynamic_wrist_rotate=0
-    joint_positions = [dynamic_waist,dynamic_shoulder ,dynamic_elbow, 0,  dynamic_wrist_angle,0]
-    #bot = InterbotixManipulatorXS("wx250s", "arm", "gripper")
+    
     bot.arm.go_to_home_pose()
-    #bot.arm.set_joint_positions(joint_positions)
+    
     sleep(2.0)
     bot.arm.set_ee_pose_components(x=0.3, z=0.2)
-    bot.arm.set_ee_pose_components(y=0.3, z=0.2)
+   
     sleep(2.0)
     bot.arm.set_single_joint_position("waist", dynamic_waist)
-    bot.arm.set_single_joint_position("shoulder", dynamic_shoulder)
-   
-    
+    #bot.arm.set_single_joint_position("shoulder", dynamic_shoulder)
     bot.gripper.open()
-    
-    #bot.arm.set_joint_positions(joint_positions)
-    #bot.arm.set_ee_pose_components(x=0.1, z=-0.16)
     bot.arm.set_ee_cartesian_trajectory(x=0.1, z=-0.16)
     bot.gripper.close()
     bot.arm.set_ee_cartesian_trajectory(x=-0.1, z=0.16)
-    #bot.arm.set_ee_pose_components(x=-0.1, z=0.16)
-    bot.arm.set_single_joint_position("waist", -np.pi/2.0)
-    #set the object position
-    #Setting the object position
-    #x, y, z = cx, cy, 0.16
-
-    # Approach the object
-    #bot.arm.set_ee_pose_components(x=x, y=y, z=z+0.05, pitch=0.5)
-    #sleep(2.0)  # Allow time for the arm to move
-
-    # Move down to the object
-    #bot.arm.set_ee_pose_components(x=x, y=y, z=z, pitch=0.5)
-    #sleep(2.0)  # Allow time for the arm to move
     
-    #bot.arm.set_ee_pose_components(x=cx/100,y=cy/100, z=0.2,pitch=0.5)
-    #bot.arm.set_ee_cartesian_trajectory(z=cy/100, y=-0.02)
+    bot.arm.set_single_joint_position("waist", -np.pi/2.0)
+   
     
     bot.arm.set_ee_cartesian_trajectory(pitch=1.5)
     bot.arm.set_ee_cartesian_trajectory(pitch=-1.5)
@@ -75,7 +48,7 @@ def perform_pick_and_place(cx, cy):
     bot.arm.set_ee_cartesian_trajectory(x=-0.1, z=0.16)
     bot.arm.go_to_home_pose()
     bot.arm.go_to_sleep_pose()
-
+    
 
 def handle_centroid(req):
     global bot, last_processed_time, processing_interval
